6328a51decdc29c84d8b52f37ad922c140eb2d36
ipramudya <ipramudya.dev@gmail.com>
2022-12-08 21:58:43 +0700

    pesan

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,4 +1,5 @@
-export const API_KEY='c6733b0d826af568ab4bfdb3b00967af'
+export const API_KEY = 'c6733b0d826af568ab4bfdb3b00967af';
 
-export const BASE_API_URL = (param) => `https://api.themoviedb.org/3/${param}?api_key=${API_KEY}`
-// export const BASE_API_URL ='https://api.themoviedb.org/3?key='
+export const BASE_API_URL = (param) =>
+  `https://api.themoviedb.org/3/${param}?api_key=${API_KEY}`;
+// export const BASE_API_URL ='https://api.themoviedb.org/3?key='....
