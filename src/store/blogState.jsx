e5cae088c8f3ad02f5755b240dc71183da01cf3b
almiraaurora <almiraprameswaty@gmail.com>
2022-12-12 22:21:01 +0700

    fix: blog movie

--- src/store/blogState.jsx
+++ src/store/blogState.jsx
@@ -28,4 +28,108 @@ export const blogState = [
         authorName: 'THEFILM.BLOG',
         createdAt: 'November 2, 2022',
     },
+
+    {
+        id: 2,
+        title: 'BLACK ADAM | REVIEW',
+        slug: 'black-adam-review',
+        image: '/assets/image/article/black-adam.webp',
+        category: 'Action, Fantasy, Science Fiction',
+        slugCategory: 'action',
+        readTime: 8,
+        description: `Johnson is Teth-Adam. He’s a Kahndaqi slave in the opening and later rediscovered beholden of the powers of Shazam. 
+        
+        Superhuman abilities bequeathed him via the very same Council of Wizards that turned Asher Angel into Zachary Levi back in 2019. 
+        
+        Though originally attached to that film, when Johnson turned down Levi’s super in favour of Black Adam – Shazam’s then villain – it was deemed a waste not to give the character his own blockbuster. 
+        
+        The logic is clear. In a tough era for the multiplex, Johnson has crowd drawing credentials. Many a likely flop has been saved by his winning smile and megawatt charisma.
+
+        Not so much here. Johnson’s Teth-Adam is appealing but dour by default. Anyone would be with this back story. Adam is a formerly oppressed slave turned demigod who emerges from three thousand years of entrapment to a world that treats him as a threat. 
+        
+        He’s an antihero with a brutal approach to dispatching ne’er do wells. There is humour in the horror – think Marvel’s deadpan Drax the Destroyer – but it’s an uneasy fit. 
+        
+        The Rock’s a broad comic, sure, but he’s warm too and twinkly and surprisingly wry. Dave Bautista makes it look easy and it’s disappointing to find Johnson less able with the delivery. 
+        
+        True, he’s not helped by a woefully inconsistent script and writing that can never quite decide how self-aware and even wise Black Adam should be, but even so.
+        <br />
+        Source: <a href='https://thefilm.blog/2022/11/08/black-adam-review/'><i>The Film Blog<i/></a>
+        `,
+        authorName: 'THEFILM.BLOG',
+        createdAt: 'November 8, 2022',
+    },
+
+    {
+        id: 3,
+        title: 'LYLE LYLE CROCODILE | REVIEW',
+        slug: 'lyle-lyle-crocodile-review',
+        image: '/assets/image/article/lyle-lyle-crocodile.webp',
+        category: 'animation, adventure, comedy',
+        slugCategory: 'animation',
+        readTime: 4,
+        description: `The hunt for the new Paddington is rife in Hollywood. Ever since the Paul King helmed franchise hit the big time, studio chiefs have had marmalade-sticky fingers all over the classics of kids lit. 
+        
+        While 2021 saw Clifford the Big Red Dog bound off Norman Bridwell’s original pages, this year brings Lyle Lyle Crocodile, an adaptation of Bernard Waber’s 1962 favourite: ‘The House on East 88th Street’, and its eponymous sequel. 
+        
+        With music from Greatest Showman maestros Pasek and Paul, Lyle has show-stopping high notes to combat a general lack of bite.
+
+        All that follows the meeting of Josh and Lyle is entirely par for the course. There’s precious little surprising in their tale, which whittles through rote expectations and predictably timely positive affirmations. 
+        
+        For youngsters, it’s enough. For grownups, it’s an undemanding way to spend a Saturday afternoon. To that end, Lyle reminds less of Paddington than Stuart Little. There’s worse company to be in.
+        <br />
+        Source: <a href='https://thefilm.blog/2022/10/22/lyle-lyle-crocodile-review/'><i>The Film Blog<i/></a>
+        `,
+        authorName: 'THEFILM.BLOG',
+        createdAt: 'October 22, 2022',
+    },
+
+    {
+        id: 4,
+        title: 'FANTASTIC BEASTS: THE SECRETS OF DUMBLEDORE | REVIEW',
+        slug: 'fantastic-beast-secrets-of-dumbledore-review',
+        image: '/assets/image/article/fantastic-beast-secrets-of-dumbledore.webp',
+        category: 'adventure, family, fantasy',
+        slugCategory: 'fantasy',
+        readTime: 4,
+        description: `There was always something pleasingly predictable about the Harry Potter films. Not least because fans came pre-versed in all that was to come, courtesy of J. K. Rowling’s phenomenally successful books. 
+        
+        The formula was a winner. Beginning, muggle and end. By contrast, Rowling’s direct-to-cinema Fantastic Beasts spin off has proved less successful. Part one was a jolly adventure but the follow up came with an excess of convolution and deficiency in logic, character and direction. 
+        
+        The landscape has shift in the years since. Episode three – The Secrets of Dumbledore – has much ground to recover. Indeed, the future of Warner Bros.’s Wizarding World might well depend on it doing just that.
+
+        It is a wise move, first and foremost, that sees Steve Kloves return to the fold as the new film’s co-writer. While its predecessors were penned by Rowling alone, the author’s cinematic inexperience was, at times, clear. 
+        
+        Kloves adapted all but one of the Potter blockbusters. The man knows a thing or two about streamlining. It shows. Secrets may not reassert Fantastic Beasts as a worthy follow up to Potter but it does at least remind that Rowling’s world of spells, potions, Quidditch and Hogwarts has mileage in it yet. 
+        
+        It’s just a pity this rambling would-be prequel pentalogy hasn’t fuel enough for the journey.
+        <br />
+        Source: <a href='https://thefilm.blog/2022/04/10/fantastic-beasts-the-secrets-of-dumbledore-review/'><i>The Film Blog<i/></a>
+        `,
+        authorName: 'THEFILM.BLOG',
+        createdAt: 'April 10, 2022',
+    },
+
+    {
+        id: 5,
+        title: 'STRANGE WORLD | REVIEW',
+        slug: 'strange-world-review',
+        image: '/assets/image/article/strange-world.webp',
+        category: 'animation, action, adventure',
+        slugCategory: 'animation',
+        readTime: 3,
+        description: `Disney seems oddly disinterested in promoting their latest toon. This being the studio’s first big budget animation since 2021’s wildly successful Encanto. Trailers have been sparse, merchandise limited and buzz low. 
+        
+        It’s a shame because Strange World is a rather fun romp on the whole. Popping visuals dance across the screen to the tune of a full bodied orchestra in swell flow. The narrative is, truth by told, somewhat lacking. It’s fabulously Gen Z in agenda but succeeds most when it tries least. 
+        
+        The throwaway progressive conceits on display resonate far more here than the heavier and worthy messaging that comes to dominate in the final stretch.
+
+        Jules Verne and the escapism of the early twentieth century pulp fiction are Strange World’s most obvious influences. There’s more than a little of Disney’s own genre dabbles in geologically infused sci-fi adventure too – Atlantis, Treasure Planet, Mars Needs Moms… That all three flopped at the box office flops perhaps explains the promotional wariness. 
+        
+        Even so, Disney’s willingness to bet once more on creative concepts over sure bets can only be seen as a pleasing benefit of the studio’s more recent winning streak.
+        <br />
+        Source: <a href='https://thefilm.blog/2022/11/24/strange-world-review/'><i>The Film Blog<i/></a>
+        `,
+        authorName: 'THEFILM.BLOG',
+        createdAt: 'November 24, 2022',
+    },
 ]
