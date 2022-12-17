88a06d9a27f753779af84c82ea14112408da1a25
Aditya Rizqi Ardhana <ardhanajames777@gmail.com>
2022-12-17 11:06:58 +0700

    fix: URL Api

--- src/api/tmbd.js
+++ src/api/tmbd.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 
 const API_KEY = 'c6733b0d826af568ab4bfdb3b00967af'
 export default axios.create({
-    baseURL: 'http://api.themoviedb.org/3',
+    baseURL: 'https://api.themoviedb.org/3',
     headers: {
+        
         Accept: 'application/json',
     },
     params: {
